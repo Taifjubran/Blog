8d6316a17530ebcf58f3f27432d49c1b0a33d488
Taif Jobran <taifjubran@gmail.com>
2020-06-06 21:29:51 -0800

    pages modification

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,8 @@
 class ArticlesController < ApplicationController
-    def show
-        @article = Article.find(params[:id])
+   
+  before_action :set_article, only: [:show,:edit, :update,:destroy] 
+  
+  def show
     end
 
     def index
@@ -8,7 +10,7 @@ class ArticlesController < ApplicationController
      end
 
      def create 
-        @article = Article.new(params.require(:article).permit(:title, :description))
+        @article = Article.new(article_params)
         if @article.save
             flash[:notice] = "Article was created successfully."
             
@@ -26,16 +28,35 @@ class ArticlesController < ApplicationController
       def edit
         @article = Article.find(params[:id])
 
+
       end
+
+
 
       def update
-        @article = Article.find(params[:id])
-       if @article.update(params.require(:article).permit(:title, :description))
-        flash[:notice] = "Article was updated successfully."
-        redirect_to @article
-       else
-        render 'edit'
-       end
-
+        if @article.update(article_params)
+          flash[:notice] = "Article was updated successfully."
+          redirect_to @article
+        else
+          render 'edit'
+        end
       end
+
+
+
+      def destroy
+        @article.destroy
+        redirect_to articles_path
+      end
+
+      private 
+      def set_article 
+        @article = Article.find(params[:id])
+      end
+      
+      def article_params
+        params.require(:article).permit(:title, :description)
+      end
+
+
     end
