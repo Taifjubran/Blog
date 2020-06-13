3ec5b1fc5df4fa69bc2903aec6c1a9be1aa524d1
Taif Jobran <taifjubran@gmail.com>
2020-06-13 13:30:06 -0800

    layout profiles pages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,16 @@
 class UsersController < ApplicationController
 
+
+
+    def show 
+        @user = User.find(params[:id])
+        @articles = @user.articles
+         end
+
+         def index
+            @users = User.all
+         end
+
     def new
         @user = User.new
     end
@@ -13,7 +24,7 @@ class UsersController < ApplicationController
         @user = User.find(params[:id])
         if @user.update(user_params)
             flash[:notice] = "Your account has been updated"
-            redirect_to articles_path
+            redirect_to @user
         else 
             render 'edit'
         end
